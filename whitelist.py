e2791961fe05e5cbb6a2a3711ca02199c3ab7f04
virus-rpi <72336443+virus-rpi@users.noreply.github.com>
2022-10-10 23:14:25 +0200

    Automated whitelist scanner

--- whitelist.py
+++ whitelist.py
@@ -1,6 +1,7 @@
 import pyautogui
 import time
 import pytesseract
+from dbManeger import dbManeger
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
@@ -19,19 +20,36 @@ def connect(server):
     time.sleep(6)
     img = pyautogui.screenshot()
     text = pytesseract.image_to_string(img)
-    if "whitelist" in text:
+    if "whitelist" in text or "white-list" in text:
         pyautogui.press('tab')
         pyautogui.press('enter')
+        print("True")
         return True
     else:
         pyautogui.press('esc')
         for i in range(7):
             pyautogui.press('tab')
         pyautogui.press('enter')
+        print("False")
         return False
 
+def getIps():
+    db = dbManeger(r"ip.db")
+    ips = db.execute('SELECT ip, port FROM ip WHERE players != "None";')
+    return ips
 
-print(connect('baum-server.de'))
-print(connect('93.186.198.89:25565'))
+def write(whitelist, nr):
+    db = dbManeger(r"ip.db")
+    db.execute(f'UPDATE ip SET whitelist = "{str(whitelist)}" WHERE ip = {nr};')
 
 
+
+ips = getIps()
+db = dbManeger(r"ip.db")
+for i in ips:
+    ip = i[0]
+    port = i[1]
+    print(f'{ip}:{port}')
+    whitelist = connect(f'{ip}:{port}')
+    nr = db.find("ip", f'"{ip}"')[0][0]
+    write(whitelist, nr)
